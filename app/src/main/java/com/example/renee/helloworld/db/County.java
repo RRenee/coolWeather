90373de9e5eb943b480f9b316d9f7e9c6c5b8934
ocmfk <renlinan@saicmotor.com>
2019-07-15 19:18:17 +0800

    省市区查询页面和交互

---
+++ app/src/main/java/com/example/renee/helloworld/db/County.java
@@ -0,0 +1,43 @@
+package com.example.renee.helloworld.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherID() {
+        return weatherID;
+    }
+
+    public void setWeatherID(String weatherID) {
+        this.weatherID = weatherID;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    private int id;
+    private  String countyName;
+    private String weatherID;
+    private int cityId;
+
+}
